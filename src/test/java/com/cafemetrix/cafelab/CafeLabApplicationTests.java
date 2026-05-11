599fb1853ba9c4b0780b0621a4e68eb25ebc20b7
Adriatic05 <175574196+adriandonayredev@users.noreply.github.com>
2026-05-10 19:02:06 -0500

    test: configure test profile with H2 database

--- src/test/java/com/cafemetrix/cafelab/CafeLabApplicationTests.java
+++ src/test/java/com/cafemetrix/cafelab/CafeLabApplicationTests.java
@@ -2,12 +2,13 @@ package com.cafemetrix.cafelab;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class CafeLabApplicationTests {
 
     @Test
     void contextLoads() {
     }
-
 }
